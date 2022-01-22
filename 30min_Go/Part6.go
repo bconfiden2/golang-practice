f0110cd1555706b25cbeb2499eac7b8db031c22e
bconfiden2 <bconfiden2@naver.com>
2022-01-22 09:23:37 +0900

    goroutine, channel

--- 30min_Go/Part6.go
+++ 30min_Go/Part6.go
@@ -28,6 +28,56 @@ func goroutine_test() {
 	time.Sleep(time.Millisecond)
 }
 
+func sum(arr []int, c chan int) {
+	fmt.Printf("%p %p\n", &arr, c)
+	sum := 0
+	for _, v := range arr {
+		sum += v
+	}
+	c <- sum
+	c <- sum+sum
+	c <- sum*3
+}
+
+func channel_test() {
+	var arr1 []int
+	for i:=0 ; i < 5 ; i++ {
+		arr1 = append(arr1, i+1)
+	}
+	arr2 := []int{10,20,30,40,50}
+
+	c := make(chan int)
+	fmt.Printf("%p %p %p\n", &arr1, &arr2, c)
+
+	arr3 := []int{100,200,300}
+	go sum(arr3, c)
+	go sum(arr2, c)
+	go sum(arr1, c)
+	x := <- c
+	y := <- c
+	z := <-c
+
+	fmt.Println(x,y,z)
+
+	ch := make(chan int)
+	go func() {ch <- 1} ()
+	fmt.Println(<-ch)
+	fmt.Println("-------")
+	
+	ch2 := make(chan int, 1)
+	
+	go func() {
+		kk := <-ch2
+		fmt.Println("go!", kk)
+		ch2 <- kk * 10
+	} ()
+	ch2 <- 1
+	fmt.Println(<-ch2)
+
+	// time.Sleep(time.Second)
+}
+
 func main() {
-	goroutine_test()
+	// goroutine_test()
+	channel_test()
 }
