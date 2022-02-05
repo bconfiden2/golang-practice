8fb2a83aec4de848ebc803a8623b83e306c25c54
bconfiden2 <bconfiden2@naver.com>
2022-02-06 07:45:05 +0900

    url parsing

---
+++ gobyexample/url.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	// "net"
+	"net/url"
+)
+
+func main() {
+	s := "postgres://user:pass@host.com:5432/path?k=v#f"
+
+	u, err := url.Parse(s)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(u.Scheme)
+	fmt.Println(u.User)
+	fmt.Println(u.User.Username())
+	fmt.Println(u.User.Password())
+	fmt.Println(u.Host)
+	fmt.Println(u.Path)
+	fmt.Println(u.Fragment)
+	fmt.Println(u.RawQuery)
+}
